c2ea877d695b88895bb056d1b2155cbb836552a3
swordfishwjy <swordfishwjy@gmail.com>
2017-12-28 10:09:56 -0600

    let start!

---
+++ edgeAssign.py
@@ -0,0 +1,120 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# 第一：假设 某个 EC_i 有 9个neighbors, 包含其本身则是10个可分配任务的nodes；
+#      本身的node使用index【0】
+# 第二：假设 当前有50个requests，等待分配给10个nodes
+# 第三：本程序所有index对应：self->node0, neighbor1 ~neighbor9 -> node1~9
+
+from gurobipy import *
+
+LIMIT = 9
+NUM_REQUESTS = 30
+
+try:
+	# Create model
+	m = Model('edgeReqAssign')
+
+	# Model data
+	# requests number
+	requests = []
+	for i in range(NUM_REQUESTS):
+		requests.append('req' + str(i))
+
+	# nodes
+	nodes = []
+	for i in range(10):
+		nodes.append('node' + str(i))
+	# the available capacity of neighbors, sum=30+4*9=66 > 50
+	capacity = {
+		'node0': 30,
+		'node1': 4,
+		'node2': 4,
+		'node3': 4,
+		'node4': 4,
+		'node5': 4,
+		'node6': 4,
+		'node7': 4,
+		'node8': 4,
+		'node9': 4
+	}
+	# the computing cost, self is 1, neighbors: 2~5 randomly
+	compCost = {
+		'node0': 1,
+		'node1': 2,
+		'node2': 3,
+		'node3': 4,
+		'node4': 5,
+		'node5': 2,
+		'node6': 3,
+		'node7': 3,
+		'node8': 5,
+		'node9': 2
+	}
+	# the link cost, self is 1, neighbors: 2~5 randomly
+	linkCost = {
+		'node0': 1,
+		'node1': 3,
+		'node2': 2,
+		'node3': 4,
+		'node4': 4,
+		'node5': 3,
+		'node6': 2,
+		'node7': 3,
+		'node8': 4,
+		'node9': 5
+	}
+	# create variables
+	choices = m.addVars(requests, nodes, vtype = GRB.BINARY, name = 'choices' )
+	totalCost = 0
+	for i, j in choices:
+		totalCost = totalCost + choices[i,j] * (compCost[j] + linkCost[j])
+
+	# set objective
+	m.setObjective(totalCost, GRB.MINIMIZE)
+	
+	# set constrains
+	# 每个request只能分配给一个node
+	for i in requests:
+		temp = 0
+		for j in nodes:
+			temp = temp + choices[i,j]
+		m.addConstr(temp == 1, i)
+
+	# 每个node被分配到任务不能超出capacity
+	for j in nodes:
+		temp = 0
+		for i in requests:
+			temp = temp + choices[i,j]
+		m.addConstr(temp <= capacity[j])
+
+	# 每个request关于cost（latency）的限制
+	for i, j in choices:
+		temp = 0
+		temp = choices[i,j] * (compCost[j] + linkCost[j])
+		m.addConstr(temp <= LIMIT)
+
+
+	# optimization
+	m.optimize()
+
+	#print result
+	varResult = m.getVars()
+	index = 0
+	print("       ", end="")
+	for i in nodes:
+		print(i.ljust(7), end="")
+	print("")
+	for i in range(NUM_REQUESTS):
+		print ('req' + str(i).ljust(2), end="    ")
+		for j in range(10):
+			print(int(abs(varResult[index].x)), end = "      ")
+			index += 1
+		print("")
+
+	print('Object: minimum cost => ', m.objVal)
+	print('Optimization Time ==>', m.Runtime)
+
+
+except GurobiError:
+	print('Error reported!')
---
+++ test.py
@@ -0,0 +1,6 @@
+requests = []
+
+for i in range(50):
+	requests.append('req' + str(i))
+
+print(requests[2])
